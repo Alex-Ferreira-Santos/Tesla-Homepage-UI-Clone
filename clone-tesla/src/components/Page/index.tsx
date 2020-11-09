88d5842e75c0f134c7a6ee8ff6a059a652101b08
Alex-Ferreira-Santos <alexfstos@gmail.com>
2020-11-09 11:30:58 -0300

    registro de modelos na context API

--- clone-tesla/src/components/Page/index.tsx
+++ clone-tesla/src/components/Page/index.tsx
@@ -24,7 +24,7 @@ const Page: React.FC = () => {
               modelName={modelName}
               overlayNode={
                 <DefaultOverlayContent 
-                  label="Model One"
+                  label={modelName}
                   description="Order Online for Delivery"
                 />
               }
